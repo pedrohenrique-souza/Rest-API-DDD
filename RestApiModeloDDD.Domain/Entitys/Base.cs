0cc413602418ad88d3615dc577743c28330d6215
Pedro Henrique <pedrohenriquez3809@outlook.com>
2026-08-17 21:06:57 -0300

    feat: adiciona projetos de dominio, infraestrutura e dbcontext

---
+++ RestApiModeloDDD.Domain/Entitys/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiModeloDDD.Domain.Entitys
+{
+    public class Base
+    {
+        public int Id { get; set; }
+    }
+}
